b4385b181651eda7bacde20caa86e5ae41a33925
zjh <34595010812@qq.com>
2023-04-12 20:26:17 +0800

    GithubSite.vus：GPT3.0

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
-    </a>
+    <span>ChatGPT 3.0</span>
   </div>
 </template>
